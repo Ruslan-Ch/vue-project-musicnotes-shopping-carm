cbde19ab90d2a8c0a1f1481351126740295f7bb3
Ruslan-Ch <ruslan.ro.com@gmail.com>
2021-05-21 14:23:23 +0300

    fixed dist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,159 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="error-invisible" v-for="(error, i) in errors"
+             :key="i"
+             :class="{ errorOrange: isActive }">
+            <h1 class="error-title">Ошибка GET запроса / {{ error }}</h1>
+        </div>
+
+        <SheetMusic :groupsArrFromSheetMusic="sheetGroupsArr"
+                    :sheetsArrFromSheetMusic="sheetsArr"
+                    @my-super-input="valueButton">
+        </SheetMusic>
+
+        <ShoppingCart :shoppingCart="shoppingCart"
+                      :userFromApp="user"
+                      @my-subtractDec="subtractDecQuantity"
+                      @my-addInc="addIncQuantity"
+                      @my-totalPrice="addTotalPrice"
+                      @my-remuve="myRemuve">
+        </ShoppingCart>
+
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SheetMusic from './components/SheetMusic.vue';
+    import ShoppingCart from './components/ShoppingCart.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            SheetMusic,
+            ShoppingCart,
+        },
+        data() {
+            return {
+                urlSheets: 'https://raw.githubusercontent.com/brightestsirius/musicnotes/master/sheets.json',
+                user: [
+                    {
+                        shipping: 0,
+                        discount: .02,
+                        coupon: .05,
+                        tax: 0,
+                        giftCard: .5
+                    }
+                ],
+                errors: [],
+                sheetCategoriesArr: [],
+                sheetGroupsArr: [],
+                sheetsArr: [],
+                shoppingCart: [],
+                isActive: false
+            }
+        },
+        async mounted() {
+            try {
+                const response = await fetch(this.urlSheets);
+                const result = await response.json();
+                this.sheetCategoriesArr = result.sheetCategories;
+                this.sheetGroupsArr = result.sheetGroups;
+                this.sheetsArr = result.sheets;
+
+                this.sheetsArr.map((item) => {
+                    this.$set(item, 'quantity', 1)
+                    this.$set(item, 'totalPrice', 0)
+
+                    item.sheetCategories.map((itemSheetCategories, indexSheetCategories, arraySheetCategories) => {
+                        this.sheetCategoriesArr.map((itemCategoriesArr, indexCategoriesArr, arrayCategoriesArr) => {
+
+                            if (arraySheetCategories[indexSheetCategories] === arrayCategoriesArr[0].id) {
+                                arraySheetCategories[indexSheetCategories] = arrayCategoriesArr[0].name
+                            } else if (arraySheetCategories[indexSheetCategories] === arrayCategoriesArr[1].id) {
+                                arraySheetCategories[indexSheetCategories] = arrayCategoriesArr[1].name
+                            }
+                        })
+                    })
+                })
+            } catch (error) {
+                this.errors.push(error)
+                setInterval(() => this.isActive = true, 700);
+                setInterval(() => this.isActive = false, 1400);
+            }
+        },
+
+        methods: {
+            valueButton(dataSheet) {
+                if (this.shoppingCart.length) {
+                    let isProductExist = false;
+                    this.shoppingCart.map((item) => {
+                        if (item.id === dataSheet.dataMusic.id) {
+                            isProductExist = true;
+                            item.quantity++
+                        }
+                    });
+
+                    if (!isProductExist) {
+                        this.shoppingCart.push(dataSheet.dataMusic)
+                    }
+                } else {
+                    this.shoppingCart.push(dataSheet.dataMusic)
+                }
+            },
+            subtractDecQuantity(index) {
+                if (this.shoppingCart[index].quantity > 1) {
+                    this.shoppingCart[index].quantity--
+                }
+            },
+            addIncQuantity(index) {
+                this.shoppingCart[index].quantity++
+            },
+            addTotalPrice(index) {
+                let a = this.shoppingCart[index].quantity,
+                    b = this.shoppingCart[index].price,
+                    c = this.shoppingCart[index].additionalPrint;
+                this.shoppingCart[index].totalPrice = a === 1 ? a * b : (a * b) - (a * c);
+            },
+            myRemuve(indexFromCart) {
+                this.shoppingCart[indexFromCart].quantity = 1
+                this.shoppingCart.splice(indexFromCart, 1)
+            },
+        }
+    }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        box-sizing: border-box;
+    }
+
+    #app {
+
+        font-family: "ProximaNova", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        /*-webkit-font-smoothing: antialiased;*/
+        /*-moz-osx-font-smoothing: grayscale;*/
+        /*text-align: center;*/
+        /*color: #2c3e50;*/
+        /*margin-top: 60px;*/
+    }
+
+    .error-invisible {
+        background: red;
+        border: 20px solid #2520ff;
+        width: 43rem;
+        height: 10rem;
+        margin: 0 auto;
+    }
+
+    .errorOrange {
+        background: #2520ff;
+        border: 20px solid red;
+    }
+
+    .error-title {
+        text-align: center;
+    }
 </style>
+
---
+++ src/components/SheetMusic.vue
@@ -0,0 +1,185 @@
+<template>
+    <div>
+        <div class="logo">
+            <img alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="musicContainer">
+
+            <div v-for="(elem, index) in groupsArrFromSheetMusic"
+                 :key="index">
+
+                <div>
+                    <p v-if="elem.id !==2">{{ elem.name}}</p>
+                </div>
+
+                <div class="sheet-music" v-if="elem.id !=2">
+
+                    <div v-for="(sheet, index) in sheetsArrFromSheetMusic"
+                         :key="index">
+
+                        <div class="sheet-music_in" v-if="sheet.sheetGroups !=2">
+                            <img class="sheet-music_img" :src="sheet.preview" alt="">
+
+                            <div class="song-desc">
+                                <p class="musicTitle">{{ sheet.title }}</p>
+                                <p class="musicArtist">{{ sheet.artist }}</p>
+                                <p class="musicCategories">{{ sheet.sheetCategories.join(", ") }}</p>
+                                <button class="buttonCart"
+                                        @click="addToCart(index, sheet)">
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <p v-if="elem.id !==1">{{ elem.name}}</p>
+                </div>
+
+                <div class="sheet-music" v-if="elem.id !=1">
+
+                    <div v-for="(sheet, index) in sheetsArrFromSheetMusic"
+                         :key="index">
+
+                        <div class="sheet-music_in" v-if="sheet.sheetGroups !=1">
+                            <img class="sheet-music_img" :src="sheet.preview" alt="">
+
+                            <div class="song-desc">
+                                <p class="musicTitle">{{ sheet.title }}</p>
+                                <p class="musicArtist">{{ sheet.artist }}</p>
+                                <p class="musicCategories">{{ sheet.sheetCategories.join(", ") }}</p>
+                                <button class="buttonCart"
+                                        @click="addToCart(index, sheet)">
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "SheetMusic",
+        props: {
+            groupsArrFromSheetMusic: {
+                type: Array,
+                default() {
+                    return {}
+                }
+            },
+            sheetsArrFromSheetMusic: {
+                type: Array,
+                default() {
+                    return {}
+                }
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            addToCart(indexIn, dataMusic) {
+                this.$emit('my-super-input', {indexIn, dataMusic});
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .logo {
+        text-align: center;
+    }
+
+    .musicContainer {
+        margin: 0 auto;
+        padding: .2rem 3rem 1rem 3rem;
+        width: 70vw;
+        background: #efefef;
+    }
+
+    .sheet-music {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .sheet-music_in {
+        --width-sheet-music_in: 10rem;
+        width: var(--width-sheet-music_in);
+        height: 13.5rem;
+        margin: 0 1rem 1rem 1rem;
+        position: relative;
+    }
+
+    .musicTitle {
+        color: #fff;
+        font-size: 14px;
+        padding-left: 10px;
+        padding-top: 6px;
+    }
+
+    .musicArtist {
+        padding-left: 10px;
+        font-weight: 400;
+        font-size: 14px;
+        color: rgba(255, 255, 255, 0.5);
+    }
+
+    .musicCategories {
+        position: absolute;
+        width: 100px;
+        padding-left: 10px;
+        font-weight: 400;
+        font-size: 14px;
+        color: rgba(255, 255, 255, 0.5);
+        bottom: 0;
+    }
+
+    .sheet-music_img {
+        width: 100%;
+        box-shadow: rgba(0, 0, 0, 0.26) 0 0 1rem 0;
+    }
+
+    .song-desc {
+        --width-song-desc: 11.5rem;
+        --width-calc: calc(var(--width-song-desc) - var(--width-sheet-music_in));
+        width: var(--width-song-desc);
+        height: 50%;
+        background-color: #0272b3;
+        border-radius: .19rem;
+        box-shadow: 0 -.125rem .44rem rgba(0, 0, 0, .33);
+        border-bottom: .31rem solid rgba(0, 0, 0, .15);
+        position: relative;
+        transform: translate(0, -100%);
+        right: calc(var(--width-calc) / 2);
+    }
+
+    .buttonCart {
+        display: inline-block;
+        float: right;
+        right: 8px;
+        bottom: 4px;
+        background-color: white;
+        color: rgb(2, 114, 179);
+        border-radius: 10px;
+        position: absolute;
+        cursor: pointer;
+        border: none;
+        padding: 5px 5px 5px 5px;
+    }
+
+    .buttonCart:focus {
+        outline: none;
+    }
+
+    .buttonCart:hover {
+        box-shadow: 0 5px 5px lightgrey;
+    }
+</style>
